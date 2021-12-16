b3d2b9293af43b950d57b1565b7a8e4d7fc6ca8d
hoba-android <heba96@yahoo.com>
2021-12-16 06:51:16 +0200

    router 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import "./App.css";
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import { Producuts } from "../src/components/data/Dummy";
+import { Producuts } from '../src/components/data/Dummy';
 
-import Header from "../src/components/UI/Header/Header";
+import Header from '../src/components/UI/Header/Header';
+import ProductsPage from './components/screens/PLP/ProductsPage';
+import ProductDescription from './components/screens/PDP/ProductDescription';
 
-import Cart from "./components/screens/Cart/Cart";
+import Cart from './components/screens/Cart/Cart';
 
 function App() {
   console.log(Producuts);
   return (
     <div className="App">
       <Header />
-      <Cart />
+      <Switch>
+        <Route exact path="/">
+          <ProductsPage />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/detials">
+          <ProductDescription />
+        </Route>
+      </Switch>
     </div>
   );
 }
